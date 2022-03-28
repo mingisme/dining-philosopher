5bcc2388f010a3a8f72865394717b273318eac68
ming.wang4 <ming.wang4@envision-digital.com>
2022-03-28 14:57:13 +0800

    deadlock

---
+++ src/App.java
@@ -0,0 +1,89 @@
+import java.util.Arrays;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class App {
+    public static void main(String[] args) {
+
+        for (int r = 0; r < 100; r++) {
+            Chopstick[] chopsticks = new Chopstick[5];
+            for (int i = 0; i < chopsticks.length; i++) {
+                chopsticks[i] = new Chopstick();
+            }
+
+            System.out.println(Arrays.toString(chopsticks));
+
+            Philosopher[] philosophers = new Philosopher[5];
+            for (int i = 0; i < philosophers.length; i++) {
+                philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % philosophers.length]);
+            }
+
+            System.out.println(Arrays.toString(philosophers));
+
+            for (int i = 0; i < philosophers.length; i++) {
+                philosophers[i].start();
+            }
+        }
+    }
+}
+
+
+class Chopstick {
+    private Lock lock;
+
+    public Chopstick() {
+        lock = new ReentrantLock();
+    }
+
+    public void pickUp() {
+        lock.lock();
+    }
+
+    public void putDown() {
+        lock.unlock();
+    }
+}
+
+class Philosopher extends Thread {
+    private int bites = 10;
+    private Chopstick left, right;
+
+    public Philosopher(Chopstick left, Chopstick right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public void eat() {
+        pickUp();
+        chew();
+        putDown();
+    }
+
+    private void putDown() {
+        right.putDown();
+        left.putDown();
+    }
+
+    private void chew() {
+        System.out.println("Thread: " + Thread.currentThread().getId() + " eat something");
+    }
+
+    private void pickUp() {
+        left.pickUp();
+        right.pickUp();
+    }
+
+    public void run() {
+        for (int i = 0; i < bites; i++) {
+            eat();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Philosopher{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
